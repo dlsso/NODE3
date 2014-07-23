7ed8780bdc424ac35aa7e12d0c73d2a90096cf2a
dlsso <supertonesfan@hotmail.com>
2014-07-22 21:45:44 -0600

    Completed requirements

--- app.js
+++ app.js
@@ -2,12 +2,26 @@ var express = require('express');
 var bodyParser = require('body-parser');
 
 var app = express();
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser());
 
+var submission = {}
+
 app.get('/', function(req, res) {
-	res.send('<h1>Hello Boulder</h1>');
+	res.send('<form method="post" action="/formsubmit"><input name="email" type="email"><input type="submit"></form>');
 });
+
+app.post('/formsubmit', function(req, res){
+	var postedData = req.body;
+	submission = postedData;
+	res.redirect('/success')
+})
+
+app.get('/success', function(req, res){
+	res.send('<h1>Success!</h1>')
+})
 
 var server = app.listen(8720, function() {
 	console.log('Express server listening on port ' + server.address().port);
